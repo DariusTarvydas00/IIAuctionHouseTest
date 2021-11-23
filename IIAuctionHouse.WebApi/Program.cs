7b506513c5cd03cc305a45eb8933824c820b8456
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-23 22:20:11 +0100

    Base Setup

---
+++ IIAuctionHouse.WebApi/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace IIAuctionHouse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
