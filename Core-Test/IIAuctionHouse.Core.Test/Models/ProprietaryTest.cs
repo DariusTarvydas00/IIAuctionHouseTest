3142778fd646c8b8ab661aaf96b7f3fc23b76e83
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-24 12:51:54 +0100

    Proprietary Test done

---
+++ Core-Test/IIAuctionHouse.Core.Test/Models/ProprietaryTest.cs
@@ -0,0 +1,72 @@
+﻿using Core.Models;
+using Xunit;
+
+namespace IIAuctionHouse.Core.Test.Models
+{
+    public class ProprietaryTest
+    {
+        private Proprietary _proprietary;
+
+        public ProprietaryTest()
+        {
+            _proprietary = new Proprietary();
+        }
+
+        // Checks if Proprietary can be initialized
+        [Fact]
+        public void Proprietary_CanBeInitialized()
+        {
+            Assert.NotNull(_proprietary);
+        }
+
+        // Checks if Proprietary Id Property exists
+        [Fact]
+        public void Proprietary_Id_Exists()
+        {
+            Assert.True(_proprietary.GetType().GetProperty("Id") != null);
+        }
+
+        // Checks if Proprietary Id is integer
+        [Fact]
+        public void Proprietary_Id_IsInteger()
+        {
+            Assert.True(_proprietary.Id is int);
+        }
+
+        // Checks if Proprietary CadastreNumber Exists
+        // Cadastre Number is unique number which gives information about property
+        [Fact]
+        public void Proprietary_CadastreNumber_Exists()
+        {
+            Assert.True(_proprietary.GetType().GetProperty("CadastreNumber") != null);
+        }
+
+        // Checks if Proprietary ForestryEnterprise propert exists
+        [Fact]
+        public void Proprietary_ForestryEnterprise_Exists()
+        {
+            Assert.True(_proprietary.GetType().GetProperty("ForestryEnterprise") != null);
+        }
+
+        // Checks if Proprietary City property exists
+        [Fact]
+        public void Proprietary_City_Exists()
+        {
+            Assert.True(_proprietary.GetType().GetProperty("City") != null);
+        }
+
+        // Checks if Proprietary GeoLocationX property exists
+        [Fact]
+        public void Proprietary_GeoLocationX_Exists()
+        {
+            Assert.True(_proprietary.GetType().GetProperty("GeoLocationX") != null);
+        }
+
+        // Checks if Proprietary GeoLocationY property exists
+        [Fact]
+        public void Proprietary_GeoLocationY_Exists()
+        {
+            Assert.True(_proprietary.GetType().GetProperty("GeoLocationY") != null);
+        }
+    }
+}
--- IIAuctionHouse.Core/Models/Proprietary.cs
+++ IIAuctionHouse.Core/Models/Proprietary.cs
@@ -3,5 +3,10 @@
     public class Proprietary
     {
         public int Id { get; set; }
+        public string CadastreNumber { get; set; }
+        public string ForestryEnterprise { get; set; }
+        public string City { get; set; }
+        public double GeoLocationX { get; set; }
+        public double GeoLocationY { get; set; }
     }
 }
