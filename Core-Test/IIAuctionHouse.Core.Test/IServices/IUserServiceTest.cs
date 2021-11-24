e047c27cf7f86bd5d76e8ef9241cf70f14f68d77
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-24 22:51:17 +0100

    Interface Admin Service Test done

---
+++ Core-Test/IIAuctionHouse.Core.Test/IServices/IUserServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Core.IServices;
+using Core.Models;
+using Moq;
+using Xunit;
+
+namespace IIAuctionHouse.Core.Test.IServices
+{
+    public class IUserServiceTest
+    {
+        [Fact]
+        public void IUserServiceTest_IsAvailable()
+        {
+            var userService = new Mock<IUserService>().Object;
+            Assert.NotNull(userService);
+        }
+
+        // Checks if GetAllUsers method return a list of all Users
+        [Fact]
+        public void GetAllUsers_WithNoParam_ReturnsListOfAllAddresses()
+        {
+            var mock = new Mock<IUserService>();
+            var fakeUserList = new List<User>();
+            mock.Setup(s => s.GetAllUsers()).Returns(fakeUserList);
+            var service = mock.Object;
+            Assert.Equal(fakeUserList, service.GetAllUsers());
+        }
+    }
+}
---
+++ IIAuctionHouse.Core/IServices/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+
+namespace Core.IServices
+{
+    public interface IUserService
+    {
+        List<User> GetAllUsers();
+    }
+}
