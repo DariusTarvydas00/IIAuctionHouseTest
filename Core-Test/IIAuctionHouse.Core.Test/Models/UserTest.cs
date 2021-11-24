5fc56bdbe1a2775f999853e9d51d4e20a7cd554a
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-24 09:20:35 +0100

    User Test

---
+++ Core-Test/IIAuctionHouse.Core.Test/Models/UserTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace IIAuctionHouse.Core.Test.Models
+{
+        public class UserTest
+        {
+            [Fact]
+            public void Test1()
+            {
+            }
+        }
+}
