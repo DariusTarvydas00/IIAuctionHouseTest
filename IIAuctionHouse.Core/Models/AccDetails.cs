f8dc627a66560b9b9bb396a79c6cf028e129b97a
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-24 12:12:18 +0100

    Account Details Test done

--- IIAuctionHouse.Core/Models/AccDetails.cs
+++ IIAuctionHouse.Core/Models/AccDetails.cs
@@ -5,6 +5,7 @@ namespace Core.Models
 {
     public class AccDetails
     {
+        public int Id { get; set; }
         public Address Address { get; set; }
         public int PhoneNumber { get; set; }
         public EmailAddressAttribute Email { get; set; }
--- IIAuctionHouse.Core/Models/Address.cs
+++ IIAuctionHouse.Core/Models/Address.cs
@@ -3,10 +3,10 @@
     public class Address
     {
         public int Id { get; set; }
-        public string City { get; set; }
         public string Country { get; set; }
-        public string StreetName { get; set; }
+        public string City { get; set; }
         public int PostCode { get; set; }
+        public string StreetName { get; set; }
         public int StreetNumber { get; set; }
     }
 }
