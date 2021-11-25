361a750b462be175c632e1b41603b619c061824f
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-25 11:47:47 +0100

    AccDetails Unit Test done done

--- Core-Test/IIAuctionHouse.Core.Test/Models/AccDetailsTest.cs
+++ Core-Test/IIAuctionHouse.Core.Test/Models/AccDetailsTest.cs
@@ -1,4 +1,6 @@
-﻿using Core.Models;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Core.Models;
 using Xunit;
 
 namespace IIAuctionHouse.Core.Test.Models
@@ -19,23 +21,25 @@ namespace IIAuctionHouse.Core.Test.Models
                 Assert.NotNull(_accDetails);
             }
 
-            // Checks if AccDetails Id  property exists
+            #region Account Details Id property Unit test
+
+            // Checks if Account Details Id property exists
             [Fact]
-            public void AccDetails_Id_Exists()
+            public void Id_NoParam_Exists()
             {
                 Assert.True(_accDetails.GetType().GetProperty("Id") != null);
             }
-            
+
             // Checks if AccDetails Id is integer
             [Fact]
-            public void AccDetails_Id_MustBeLong()
+            public void Id_NoParam_MustBeLong()
             {
                 Assert.True(_accDetails.Id is int);
             }
             
             // Checks if AccDetails Id can be assigned
             [Fact]
-            public void AccDetails_SetId_StoresId()
+            public void Id_SetId_StoresId()
             {
                 _accDetails.Id = 1;
                 Assert.Equal(1, _accDetails.Id);
@@ -43,39 +47,109 @@ namespace IIAuctionHouse.Core.Test.Models
             
             // Checks if AccDetails Id can be updated
             [Fact]
-            public void AccDetails_UpdateId_StoresNewId()
+            public void Id_UpdateId_StoresNewId()
             {
                 _accDetails.Id = 1;
                 _accDetails.Id = 2;
                 Assert.Equal(2, _accDetails.Id);
             }
+            #endregion
             
-            // Checks if Address property exists
+            #region Account Details PhoneNumber property Unit test
+
+            // Checks if Account Details PhoneNumber property exists
             [Fact]
-            public void AccDetails_Address_Exists()
+            public void PhoneNumber_NoParam_Exists()
             {
-                Assert.True(_accDetails.GetType().GetProperty("Address") != null);
+                Assert.True(_accDetails.GetType().GetProperty("Id") != null);
             }
 
-            // Checks if PhoneNumber property exists
+            // Checks if AccDetails PhoneNumber property is integer
             [Fact]
-            public void AccDetails_PhoneNumber_Exists()
+            public void PhoneNumber_NoParam_IsInteger()
             {
-                Assert.True(_accDetails.GetType().GetProperty("PhoneNumber") != null);
+                Assert.True(_accDetails.Id is int);
             }
             
+            // Checks if AccDetails PhoneNumber property can be stored
+            [Fact]
+            public void PhoneNumber_SetPhoneNumber_StoresPhoneNumber()
+            {
+                _accDetails.PhoneNumber = 123456789;
+                Assert.Equal(123456789, _accDetails.PhoneNumber);
+            }
+            
+            // Checks if AccDetails PhoneNumber property can be updated
+            [Fact]
+            public void PhoneNumber_UpdatePhoneNumber_StoresNewPhoneNumber()
+            {
+                _accDetails.PhoneNumber = 987654321;
+                _accDetails.PhoneNumber = 123456789;
+                Assert.Equal(123456789, _accDetails.PhoneNumber);
+            }
+            #endregion
+
+            #region Account Details Email property Unit Test
+
             //Checks if Email property exists
             [Fact]
-            public void AccDetails_Email_Exists()
+            public void Email_NoParam_Exists()
             {
                 Assert.True(_accDetails.GetType().GetProperty("Email") != null);
             }
+
+            // Checks if Email property is stored
+            [Fact]
+            public void Email_SetEmail_StoresEmail()
+            {
+                _accDetails.Email = "test@test.com";
+                Assert.Equal("test@test.com", _accDetails.Email);
+            }
             
-             //Checks if AccCreationDate property exists
-             [Fact]
-            public void AccDetails_AccCreationDate_Exists()
+            // Checks if Email property is updated
+            [Fact]
+            public void Email_UpdateEmail_UpdatesEmail()
+            {
+                _accDetails.Email = "test@test.com";
+                _accDetails.Email = "test2@test2.com";
+                Assert.Equal("test2@test2.com", _accDetails.Email);
+            }
+
+            #endregion
+
+            #region Account Details AccCreationDate property Unit Test
+            
+            //Checks if AccCreationDate property exists
+            [Fact]
+            public void AccCreationDate_NoParam_Exists()
             {
                 Assert.True(_accDetails.GetType().GetProperty("AccCreationDate") != null);
+            }
+            
+            // Checks if Account Details AccCreationDate property is stored
+            [Fact]
+            public void AccCreationDate_SetAccCreationDate_StoresDate()
+            {
+                _accDetails.AccCreationDate = new DateTime(2021,11, 24);
+                Assert.Equal(new DateTime(2021,11,24), _accDetails.AccCreationDate);
+            }
+
+            // Checks if Account Details AccCreationDate property is updated
+            [Fact]
+            public void AccCreationDate_UpdateAccCreationDate_UpdatesDate()
+            {
+                _accDetails.AccCreationDate = new DateTime(2021,11, 24);
+                _accDetails.AccCreationDate = new DateTime(2021,11, 25);
+                Assert.Equal(new DateTime(2021,11,25), _accDetails.AccCreationDate);
+            }
+
+            #endregion
+
+            // Checks if Address property exists
+            [Fact]
+            public void Address_NoParam_Exists()
+            {
+                Assert.True(_accDetails.GetType().GetProperty("Address") != null);
             }
     }
 }
